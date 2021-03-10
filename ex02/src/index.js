d660cebca3e351bb1937d5aab19311fa7ae2b417
mbabic-arena <m_babic@teol.net>
2021-03-10 13:58:18 +0100

    Added folder ex02 and folder src with a file index.js

---
+++ ex02/src/index.js
@@ -0,0 +1,14 @@
+function main() {
+    // Only change code below this line
+    // This is a single line JS comment
+    /* This is multi line comment!
+       This is multi line comment!
+       This is multi line comment!
+       This is multi line comment!
+       */
+    // Only change code above this line
+}
+
+console.log(main());
+
+module.exports = main;
