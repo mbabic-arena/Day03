2a8f40f8b35d96d88a7b88398eed6060bc0d4235
mbabic-arena <m_babic@teol.net>
2021-03-14 15:56:58 +0100

    Corrected exercises ex01 and ex06

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,9 +1,9 @@
 function main() {
-    // Only change code below this line
-    console.log ('Hello World!');
-    // Only change code above this line
+  // Only change code below this line
+  console.log("Hello World!");
+  // Only change code above this line
 }
 
 main();
 
-module.export = main;
+module.exports = main;
--- ex06/src/index.js
+++ ex06/src/index.js
@@ -1,17 +1,16 @@
 function main() {
-    // Only change code below this line
-    var x = 7;
-    var y = 12;
-    var z = "My name is"
-    // Only change code above this line
-    x = x + 2;
+  // Only change code below this line
+  var x = 7;
+  var y = 12;
+  var z = "My name is";
+  // Only change code above this line
+  x = x + 2;
 
-    y = y + 4;
+  y = y + 4;
 
-    z = z + " John!"
+  z = z + " John!";
 
-    return [x,  y, z];
-    
+  return [x, y, z];
 }
 
 console.log(main());
